45aaa79e40bef9cd4d1535fbfcf2e8289d00b0c4
serii <seriiburduja@gmail.com>
2022-02-01 20:31:56 +0200

    feat: backend files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,13 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="{{ mix('js/app.js', 'build') }}" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css', 'build') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,34 @@
 const mix = require('laravel-mix');
+// mix.js('resources/js/app.js', 'public/js')
+//     .version()
+//     .vue();
+// mix.sass('resources/sass/app.scss', 'css').version();
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+let productionSourceMaps = false;
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.setPublicPath('public/build')
+    .setResourceRoot('build')
+    .js('resources/js/app.js', 'js').vue()
+    .sass('resources/sass/app.scss', 'css')
+    .version()
+    .options({
+        processCssUrls: false
+    })
+    .sourceMaps(productionSourceMaps, 'source-map')
+    .browserSync({
+        proxy: 'https://localhost:8088/',
+        files: [
+            'public/build/css/app.css',  // Generated .css file
+            'public/build/js/app.js',    // Generated .js file
+            'app/**/*.+(html|php)',          // Generic .html and/or .php files [no specific platform]
+            'resources/js/**/*.vue',          // Generic .html and/or .php files [no specific platform]
+            'resources/views/**/*.php', // Laravel-specific view files
+        ],
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0'
+            }
+        },
+        open: false
+    });
